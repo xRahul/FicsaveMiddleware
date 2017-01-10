d45cb8ce7ef7eae5978000c3e321c1f6d9c51608
Rahul Jain <rahul_jain@live.in>
2017-01-10 09:38:15 +0530

    Refactoring of download listener

--- app/src/main/java/in/rahulja/ficsavemiddleware/MainActivity.java
+++ app/src/main/java/in/rahulja/ficsavemiddleware/MainActivity.java
@@ -3,23 +3,16 @@ package in.rahulja.ficsavemiddleware;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.DownloadManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.Patterns;
-import android.webkit.CookieManager;
-import android.webkit.DownloadListener;
-import android.webkit.URLUtil;
 import android.webkit.ValueCallback;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -35,59 +28,45 @@ public class MainActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
     Activity mActivity;
 
-    private String downloadUrl;
-    private String downloadUserAgent;
-    private String downloadContentDisposition;
-    private String downloadMimeType;
     private String ficUrl;
 
-    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
         mActivity = this;
-
         setFicUrl(getIntent());
 
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(mActivity,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+        } else {
+            init();
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void init() {
         final WebView mWebview = (WebView) findViewById(R.id.main_webview);
 
         if (mWebview != null) {
-            mWebview.setDownloadListener(new DownloadListener() {
-                public void onDownloadStart(String url, String userAgent,
-                                            String contentDisposition, String mimetype,
-                                            long contentLength) {
-                    
-                    downloadUrl = url;
-                    downloadUserAgent = userAgent;
-                    downloadContentDisposition = contentDisposition;
-                    downloadMimeType = mimetype;
-
-                    if (ContextCompat.checkSelfPermission(getApplicationContext(),
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                            != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(mActivity,
-                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
-                    } else {
-                        downloadFile();
-                    }
-                }
-            });
+            mWebview.setDownloadListener(new MyDownloadListener(this));
 
             mWebview.setWebViewClient(new WebViewClient() {
                 public void onPageFinished(WebView view, String url) {
                     if (!url.contains("http://ficsave.xyz/download/") && !ficUrl.isEmpty()) {
                         final String jsString =
-                            "document.getElementById('url').value = \"" + ficUrl + "\"; " +
-                            "document.getElementsByClassName('select-dropdown')[0].value = \"MOBI\";" +
-                            "document.getElementsByClassName('dropdown-content select-dropdown')[0].getElementsByTagName('li')[0].className = \"\";" +
-                            "document.getElementsByClassName('dropdown-content select-dropdown')[0].getElementsByTagName('li')[1].className = \"active\";" +
-                            "document.getElementsByName('format')[0].value = \"mobi\";" +
-                            "document.getElementById(\"download-submit\").click();";
+                                "document.getElementById('url').value = \"" + ficUrl + "\"; " +
+                                        "document.getElementsByClassName('select-dropdown')[0].value = \"MOBI\";" +
+                                        "document.getElementsByClassName('dropdown-content select-dropdown')[0].getElementsByTagName('li')[0].className = \"\";" +
+                                        "document.getElementsByClassName('dropdown-content select-dropdown')[0].getElementsByTagName('li')[1].className = \"active\";" +
+                                        "document.getElementsByName('format')[0].value = \"mobi\";" +
+                                        "document.getElementById(\"download-submit\").click();";
 
                         final Handler handler = new Handler();
                         handler.postDelayed(new Runnable() {
@@ -97,9 +76,9 @@ public class MainActivity extends AppCompatActivity {
                                     @Override
                                     public void onReceiveValue(String value) {
                                         Toast.makeText(
-                                            getApplicationContext(),
-                                            value,
-                                            LENGTH_SHORT
+                                                getApplicationContext(),
+                                                "Script run successfully!",
+                                                LENGTH_SHORT
                                         ).show();
                                     }
                                 });
@@ -140,9 +119,7 @@ public class MainActivity extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    downloadFile();
-
+                    init();
                 } else {
                     Toast.makeText(
                         getApplicationContext(), 
@@ -152,34 +129,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-    }
-
-    private void downloadFile() {
-        String url = downloadUrl;
-        String userAgent = downloadUserAgent;
-        String contentDisposition = downloadContentDisposition;
-        String mimetype = downloadMimeType;
-
-        String fileName = URLUtil.guessFileName(url, contentDisposition, mimetype);
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-
-        request.allowScanningByMediaScanner();
-        request.setNotificationVisibility(
-                DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED
-        ); //Notify client once download is completed!
-        request.setDestinationInExternalPublicDir(
-                Environment.DIRECTORY_DOWNLOADS, fileName);
-        request.setMimeType(mimetype);
-        request.setTitle(fileName);
-
-        String cookies = CookieManager.getInstance().getCookie(url);
-        request.addRequestHeader("cookie", cookies);
-        request.addRequestHeader("User-Agent", userAgent);
-
-        DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-        dm.enqueue(request);
-
-        Toast.makeText(getApplicationContext(), "Downloading File", //To notify the Client that the file is being downloaded
-                Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/in/rahulja/ficsavemiddleware/MyDownloadListener.java
@@ -0,0 +1,138 @@
+package in.rahulja.ficsavemiddleware;
+
+import android.app.DownloadManager;
+//import android.content.ActivityNotFoundException;
+//import android.content.BroadcastReceiver;
+import android.content.Context;
+//import android.content.Intent;
+//import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Environment;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
+import android.widget.Toast;
+
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+
+class MyDownloadListener implements DownloadListener {
+    private Context mContext;
+    private DownloadManager mDownloadManager;
+//    private long mDownloadedFileID;
+//    private DownloadManager.Request mRequest;
+
+    private String downloadUrl;
+    private String downloadUserAgent;
+    private String downloadContentDisposition;
+    private String downloadMimeType;
+
+    MyDownloadListener(Context context) {
+        mContext = context;
+        mDownloadManager = (DownloadManager) mContext
+                .getSystemService(Context.DOWNLOAD_SERVICE);
+    }
+
+    @Override
+    public void onDownloadStart(String url, String userAgent, String
+            contentDisposition, final String mimetype, long contentLength) {
+
+        downloadUrl = url;
+        downloadUserAgent = userAgent;
+        downloadContentDisposition = contentDisposition;
+        downloadMimeType = mimetype;
+
+        downloadFile();
+
+        /*
+
+        // Function is called once download completes.
+        BroadcastReceiver onComplete = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // Prevents the occasional unintentional call. I needed this.
+                if (mDownloadedFileID == -1)
+                    return;
+                Intent fileIntent = new Intent(Intent.ACTION_VIEW);
+
+                // Grabs the Uri for the file that was downloaded.
+                Uri mostRecentDownload =
+                        mDownloadManager.getUriForDownloadedFile(mDownloadedFileID);
+                // DownloadManager stores the Mime Type. Makes it really easy for us.
+                String mimeType =
+                        mDownloadManager.getMimeTypeForDownloadedFile(mDownloadedFileID);
+                fileIntent.setDataAndType(mostRecentDownload, mimeType);
+                fileIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                try {
+                    mContext.startActivity(fileIntent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(mContext, "No handler for this type of file.",
+                            Toast.LENGTH_LONG).show();
+                }
+                // Sets up the prevention of an unintentional call. I found it necessary. Maybe not for others.
+                mDownloadedFileID = -1;
+            }
+        };
+        // Registers function to listen to the completion of the download.
+        mContext.registerReceiver(onComplete, new
+                IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
+
+
+        mRequest = new DownloadManager.Request(Uri.parse(url));
+        // Limits the download to only over WiFi. Optional.
+        mRequest.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI);
+        // Makes download visible in notifications while downloading, but disappears after download completes. Optional.
+        mRequest.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
+        mRequest.setMimeType(mimetype);
+
+        // If necessary for a security check. I needed it, but I don't think it's mandatory.
+        String cookie = CookieManager.getInstance().getCookie(url);
+        mRequest.addRequestHeader("Cookie", cookie);
+
+        // Grabs the file name from the Content-Disposition
+        String filename = null;
+        Pattern regex = Pattern.compile("(?<=filename=\").*?(?=\")");
+        Matcher regexMatcher = regex.matcher(contentDisposition);
+        if (regexMatcher.find()) {
+            filename = regexMatcher.group();
+        }
+
+        // Sets the file path to save to, including the file name. Make sure to have the WRITE_EXTERNAL_STORAGE permission!!
+        mRequest.setDestinationInExternalFilesDir(mContext, Environment.DIRECTORY_DOWNLOADS, filename);
+        // Sets the title of the notification and how it appears to the user in the saved directory.
+        mRequest.setTitle(filename);
+
+        // Adds the request to the DownloadManager queue to be executed at the next available opportunity.
+        mDownloadedFileID = mDownloadManager.enqueue(mRequest);
+        */
+    }
+
+    private void downloadFile() {
+        String url = downloadUrl;
+        String userAgent = downloadUserAgent;
+        String contentDisposition = downloadContentDisposition;
+        String mimetype = downloadMimeType;
+
+        String fileName = URLUtil.guessFileName(url, contentDisposition, mimetype);
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+
+        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(
+                DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED
+        ); //Notify client once download is completed!
+        request.setDestinationInExternalPublicDir(
+                Environment.DIRECTORY_DOWNLOADS, fileName);
+        request.setMimeType(mimetype);
+        request.setTitle(fileName);
+
+        String cookies = CookieManager.getInstance().getCookie(url);
+        request.addRequestHeader("cookie", cookies);
+        request.addRequestHeader("User-Agent", userAgent);
+
+        mDownloadManager.enqueue(request);
+
+        Toast.makeText(mContext, "Downloading File", //To notify the Client that the file is being downloaded
+                Toast.LENGTH_LONG).show();
+    }
+}
